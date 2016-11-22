ceaf7cb8f265a88a0292655a3003901c50f5d098
anamikagit <anamika.arya1990@gmail.com>
2016-11-22 04:50:47 -0800

    Adding activity,rest,adapter,model packages

---
+++ app/src/main/java/com/example/aarya/eventmanager/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aarya.eventmanager.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aarya.eventmanager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
