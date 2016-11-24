f177b37c59d96a4c2780d581ed061eae68b047dc
anamikagit <anamika.arya1990@gmail.com>
2016-11-24 00:20:01 -0800

    Printing IMEI num and date time to toast

--- app/src/main/java/com/example/aarya/eventmanager/activity/MainActivityTimeIn.java
+++ app/src/main/java/com/example/aarya/eventmanager/activity/MainActivityTimeIn.java
@@ -1,11 +1,14 @@
 package com.example.aarya.eventmanager.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.aarya.eventmanager.R;
 import com.example.aarya.eventmanager.adapter.GuardListAdapter;
@@ -13,6 +16,10 @@ import com.example.aarya.eventmanager.model.Guards;
 import com.example.aarya.eventmanager.rest.ApiClient;
 import com.example.aarya.eventmanager.rest.ApiInterface;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -21,11 +28,22 @@ import retrofit2.Response;
 
 public class MainActivityTimeIn extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_time_in);
 
+        // display IMEI num and date time(24-nov-2016)
+        String deviceNum;
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        deviceNum = telephonyManager.getDeviceId();
+        //String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm");
+        String strDate = sdf.format(c.getTime());
+        Toast.makeText(MainActivityTimeIn.this, deviceNum+" "+strDate,Toast.LENGTH_LONG).show();
+        //Recycler View
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -42,8 +60,9 @@ public class MainActivityTimeIn extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Guards>> call, Throwable t) {
-                Log.e(TAG, "Call failed: "+t.getMessage());
+                Log.e(TAG, "Call failed: " + t.getMessage());
             }
         });
+
     }
 }
